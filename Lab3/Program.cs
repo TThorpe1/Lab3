278d0d24d470e5d8d3542b02c97b5c25d96490b8
terri <thorpetann@gmail.com>
2018-10-19 19:16:54 -0400

    excluded letters

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -20,37 +20,42 @@ namespace Lab3
                 Console.WriteLine("Please enter your name");
                 name = Console.ReadLine();
 
-                Console.WriteLine("Please enter an integer between 1 and 100");
-                number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter an integer between 1 and 100.");
+                string input = Console.ReadLine();
 
-                if (number >= 100 || number <= 1 )
+                if (!int.TryParse(input, out number))
                 {
-                    Console.WriteLine(name + " your number is not in the given range. Please try again!");
+                    Console.WriteLine(name +","+" you did not enter an integer!!");
+                }
+
+                else if (number >= 100 || number <= 1 )
+                {
+                    Console.WriteLine(name +","+ " your number is not in the given range. Please try again!");
                 }
                  
                 else if
                    ((number % 2 == 0) & (number >= 2) & (number <= 25))
                 {
-                    Console.WriteLine(name + " the number is even and less than 25");
+                    Console.WriteLine(name +","+ " the number is even and less than 25.");
                 }
                 else if
                     ((number % 2 == 0) & (number >= 26) & (number <= 60))
                 {
-                    Console.WriteLine(name + " the number is even");
+                    Console.WriteLine(name +","+ " the number is even.");
                 }
 
                 else if
                 ((number % 2 == 0) & (number > 60) & (number < 100))
 
                 {
-                    Console.WriteLine(name + number + " is even");
+                    Console.WriteLine(name +", " + number + " is even.");
                 }
 
                 else if
                     (number % 2 != 0)
 
                 {
-                    Console.WriteLine(name +", " +  number + " is odd");
+                    Console.WriteLine(name +", " +  number + " is odd.");
                 }
 
                 Console.WriteLine("Do you want to continue \"(y/n)\"?");
