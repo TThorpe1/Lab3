9fc4c426538e8fd44176d0bbcb40dee148e865c1
Jakyle <jjackson360@gmail.com>
2018-10-21 14:22:43 -0400

    James TA Review

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab3
 {
@@ -10,14 +6,23 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
+            // James - You don't have to do this line at all, you can just do your while loop
+            // and say have it's condition be true, so...
+            // while (true)
             string userChoice = "y";
+
+            // James = I like that you are using string.ToLower() in this line, but I would maybe 
+            // use string.Equals("y", StrinComparison.OrdinalCase)
             while (userChoice.ToLower() == "y")
             {
-
+                // James - I would define this number closer to where the varialbe is 
                 int number;
                 string name;
 
                 Console.WriteLine("Please enter your name");
+
+                // James - I would initialize the variable where it's being defined here, 
+                // string name = Console.ReadLine();
                 name = Console.ReadLine();
 
                 Console.WriteLine("Please enter an integer between 1 and 100.");
@@ -25,37 +30,40 @@ namespace Lab3
 
                 if (!int.TryParse(input, out number))
                 {
-                    Console.WriteLine(name +","+" you did not enter an integer!!");
+                    Console.WriteLine(name + "," + " you did not enter an integer!!");
                 }
 
-                else if (number >= 100 || number <= 1 )
+                // James - currently your program does not work correctly for 
+                // number 100 and number 1.  I would change your condition to make sure
+                // you are doing something for number 1 and number 100.
+                else if (number >= 100 || number <= 1)
                 {
-                    Console.WriteLine(name +","+ " your number is not in the given range. Please try again!");
+                    Console.WriteLine(name + "," + " your number is not in the given range. Please try again!");
                 }
-                 
+
                 else if
                    ((number % 2 == 0) & (number >= 2) & (number <= 25))
                 {
-                    Console.WriteLine(name +","+ " the number is even and less than 25.");
+                    Console.WriteLine(name + "," + " the number is even and less than 25.");
                 }
                 else if
                     ((number % 2 == 0) & (number >= 26) & (number <= 60))
                 {
-                    Console.WriteLine(name +","+ " the number is even.");
+                    Console.WriteLine(name + "," + " the number is even.");
                 }
 
                 else if
                 ((number % 2 == 0) & (number > 60) & (number < 100))
 
                 {
-                    Console.WriteLine(name +", " + number + " is even.");
+                    Console.WriteLine(name + ", " + number + " is even.");
                 }
 
                 else if
                     (number % 2 != 0)
 
                 {
-                    Console.WriteLine(name +", " +  number + " is odd.");
+                    Console.WriteLine(name + ", " + number + " is odd.");
                 }
 
                 Console.WriteLine("Do you want to continue \"(y/n)\"?");
